14f3419e61ea678bc06c9040b8e3da626bead713
Hassan Alhassan <alhassanh88@gmail.com>
2023-08-14 00:54:05 +0100

    b: ui bootstrap auth

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
     $page_name = 'analytics';
     return view('annoucements', compact('page_name'));
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
